63b6cb338859df8b31148c1f86f4a5d820d79af4
Nikolaos Liliakidis <nickliliakides@gmail.com>
2018-01-22 01:21:56 +0000

    Add files via upload

---
+++ js/maps.js
@@ -0,0 +1,12 @@
+  function initMap() {
+        var uluru = {lat: 40.609952, lng: 22.989573};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
